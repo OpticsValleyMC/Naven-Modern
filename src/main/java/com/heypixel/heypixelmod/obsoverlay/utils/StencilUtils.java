d266205b0df17ae51fd8a38b19e6c4a2729635d6
OpticsValleyMC <138211264+OpticsValleyMC@users.noreply.github.com>
2026-03-03 12:15:11 +0800

    Update StencilUtils.java

--- src/main/java/com/heypixel/heypixelmod/obsoverlay/utils/StencilUtils.java
+++ src/main/java/com/heypixel/heypixelmod/obsoverlay/utils/StencilUtils.java
@@ -9,9 +9,14 @@ import org.lwjgl.opengl.GL11;
 
 public class StencilUtils {
    private static final Minecraft mc = Minecraft.getInstance();
+   private static int stencilDepthBufferID = -1;
 
    public static void write(boolean renderClipLayer) {
       setupFBO();
+      if (stencilDepthBufferID != -1) {
+         EXTFramebufferObject.glFramebufferRenderbufferEXT(36160, 36128, 36161, stencilDepthBufferID);
+         EXTFramebufferObject.glFramebufferRenderbufferEXT(36160, 36096, 36161, stencilDepthBufferID);
+      }
       GL11.glClear(1024);
       GL11.glEnable(2960);
       GL11.glStencilFunc(519, 1, 65535);
@@ -32,15 +37,20 @@ public class StencilUtils {
    }
 
    public static void setupFBO() {
-      if (mc.getMainRenderTarget().getDepthTextureId() > -1) {
+      if (stencilDepthBufferID == -1 || mc.getMainRenderTarget().getDepthTextureId() > -1) {
          setupFBO(mc.getMainRenderTarget());
          ((RenderTargetAccessor)mc.getMainRenderTarget()).setDepthBufferId(-1);
       }
    }
 
    public static void setupFBO(RenderTarget fbo) {
-      EXTFramebufferObject.glDeleteRenderbuffersEXT(fbo.getDepthTextureId());
+      if (fbo.getDepthTextureId() > -1) {
+         EXTFramebufferObject.glDeleteRenderbuffersEXT(fbo.getDepthTextureId());
+      } else if (stencilDepthBufferID > -1) {
+         EXTFramebufferObject.glDeleteRenderbuffersEXT(stencilDepthBufferID);
+      }
       int stencilDepthBufferID = EXTFramebufferObject.glGenRenderbuffersEXT();
+      StencilUtils.stencilDepthBufferID = stencilDepthBufferID;
       EXTFramebufferObject.glBindRenderbufferEXT(36161, stencilDepthBufferID);
       EXTFramebufferObject.glRenderbufferStorageEXT(36161, 34041, mc.getWindow().getWidth(), mc.getWindow().getHeight());
       EXTFramebufferObject.glFramebufferRenderbufferEXT(36160, 36128, 36161, stencilDepthBufferID);
